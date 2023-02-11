1417c505b554669c4cc3a4bcc02cf6f1c10b7277
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-02-12 01:30:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
 const express = require('express');
-const cookieParser = require('cookie-parser'); 
+const cookieParser = require('cookie-parser');
 
 const PORT = process.env.PORT || 3001;
 const router = require('./src/router');
-const {dbInit} = require('./src/config/db.js');
+const { dbInit } = require('./src/config/db.js');
 //const { errorHandler } = require('./middlewares/errorHandlerMiddleware');
-  
 
-const app = express();                                                                                         
 
-app.use(express.urlencoded({extended: false}));
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.json());
 //app.use(auth); 
@@ -18,9 +18,9 @@ app.use(router);
 
 
 dbInit()
-.then(()=>{
-    app.listen(PORT, ()=> console.log(`Server is listening on port: ${PORT}`))
-})
-.catch(err=>{
-    console.log('Application failed:', err);
-});
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`))
+    })
+    .catch(err => {
+        console.log('Application failed:', err);
+    });
