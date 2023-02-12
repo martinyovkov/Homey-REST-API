1825ae17b24e68a616870c2f8f7cbb8ba04e08b1
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-02-12 17:47:45 +0200

    feat: initialize propertyController

---
+++ src/controllers/propertyController.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+const propertyService = require('../services/propertyService');
+
+module.exports = router;
--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
+
 const userController = require('./controllers/userController');
+const propertyController = require('./controllers/propertyController');
 
 router.use(userController);
+router.use('/properties', propertyController);
 
 module.exports = router;
