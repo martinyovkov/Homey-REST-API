3dbd9b326bc83e526c858a781e19d4c1190edb66
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-02-15 14:40:33 +0200

    up

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 
 require('dotenv').config();
-console.log(process.env);
 
 const PORT = process.env.PORT || 3001;
 
